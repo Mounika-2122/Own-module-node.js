54fb9e7cf7ef9b5cf3b5eee941302adda2bee6c6
Mounika <bojjamounika8@gmail.com>
2022-01-05 16:47:22 +0530

    ownmodule

---
+++ ownmodule.js
@@ -0,0 +1,7 @@
+var calculator = require('./ownmodulecalcr.js')
+var x=20 , y=20;
+
+console.log("Addition of 20 and 20 is" + calculator.add(x,y));
+console.log("subtraction of 20 and 20 is" + calculator.sub(x,y));
+console.log("Multiplication of 20 and 20 is" + calculator.mul(x,y));
+console.log("Division of 20 and 20 is" + calculator.div(x,y));
